3c87e2cdb679e9040778a1369aed16ae6689f0b5
Nasiruddin <nasir@programmer.net>
2015-01-14 22:39:19 +0530

    layout
    make layout for CRM for designing

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,26 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller{
+    
+        public function __construct() {
+            parent::__construct();
+            
+        }
+        function layout(){
+            
+        }
+        
+        function menue(){
+            
+        }
+        
+	function header(){
+            
+        }
+        function footer(){
+            
+        }
+            
+        
+}
+
