c3d3e12680bf78b138bd072681bbb4c78812ea5d
Nasiruddin <nasir@programmer.net>
2015-01-14 18:33:44 +0530

    6.11  pm 1/14/2015 Modify by Nasiruddin. Signed-off-by:Nasiruddin <nasir@programmer.net>

---
+++ application/controllers/blog.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class blog extends CI_Controller {
+
+
+	public function index()
+	{
+            //$this->load->view('welcome_message');
+           /*// hello display here for requesting file*/
+            echo "only for education purpose";
+	}
+}
+
+?>
+
+
+
